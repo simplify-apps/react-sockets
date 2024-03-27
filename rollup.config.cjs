29c7e72bee1580e905797ba7222fc565bca73cab
simplify-apps <simplify-apps@outlook.com>
2024-03-27 11:49:06 +0300

    Add new files and configurations

---
+++ rollup.config.cjs
@@ -0,0 +1,36 @@
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+
+const pkg = require("./package.json");
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({
+      exclude: "**/__tests__/**",
+      clean: true,
+    }),
+    commonjs({
+      include: ["node_modules/**"],
+      namedExports: {
+        "node_modules/react/index.js": ["useState", "useRef", "useEffect"],
+      },
+    }),
+    terser(),
+  ],
+};
