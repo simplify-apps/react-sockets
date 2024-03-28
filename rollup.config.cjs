bc783ea498bc12fa076c81a53afc5eeddf3b91c6
simplify-apps <simplify-apps@outlook.com>
2024-03-28 18:48:01 +0300

    Update package.json and rollup.config.cjs, add closeAllSingnalrConnections and restartAllSingalrConnections functions

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,34 +1,34 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import terser from "@rollup/plugin-terser";
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
+      format: 'cjs',
+      exports: 'named',
       sourcemap: true,
     },
     {
       file: pkg.module,
-      format: "es",
-      exports: "named",
+      format: 'es',
+      exports: 'named',
       sourcemap: true,
     },
   ],
   plugins: [
     typescript({
-      exclude: "**/__tests__/**",
+      exclude: '**/__tests__/**',
       clean: true,
     }),
     commonjs({
-      include: ["node_modules/**"],
+      include: ['node_modules/**'],
       namedExports: {
-        "node_modules/react/index.js": ["useState", "useRef", "useEffect"],
+        'node_modules/react/index.js': ['useState', 'useRef', 'useEffect'],
       },
     }),
     terser(),
